341772725d6f8c2c43783b9fd62a335641379a29
Danielnk10 <144683027+Bruno007k@users.noreply.github.com>
2023-10-24 10:27:56 -0300

    questoes
    prof sergio

---
+++ par e impar.py
@@ -0,0 +1,6 @@
+n1 = int(input('digite um numero'))
+r = n1%2
+if r == 0:
+    print('o numero {} que vc digitou é par'.format(n1))
+else:
+    print('seu numero {} que vc digitou é impar'.format(n1))
